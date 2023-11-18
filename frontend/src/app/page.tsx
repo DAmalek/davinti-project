b5980662e0cebdeda467959f7f40ee7494306cf5
Daniel Abdel <danielmalek101@gmail.com>
2023-11-18 12:36:38 -0300

    axios install

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,62 @@
+"use client";
 import { Header } from "@/components/Header";
-import Image from "next/image";
+import { LinkBtn } from "@/components/LinkBtn";
+import axios from "axios";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [form, setForm] = useState({});
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5000/contacts");
+    } catch (error) {}
+  }
+
+  //console.log(form);
+
   return (
     <>
-      <Header title="Davint App" />
+      <Header title="Davint App - Agenda Telefônica" />
+      <form className="flex flex-col gap-3">
+        <input
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 "
+          placeholder="Nome"
+          name="NOME"
+          type="text"
+          onChange={handleChange}
+        />
+        <input
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 "
+          placeholder="Idade"
+          name="IDADE"
+          type="text"
+          onChange={handleChange}
+        />
+        <input
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 focus-within:border-slate-100 "
+          placeholder="Número"
+          name="NUMERO"
+          type="text"
+          onChange={handleChange}
+        />
+
+        <div className=" flex mt-6">
+          <LinkBtn title="Pesquisar" path="/" />
+          <button
+            className=" w-1/2 border border-slate-300 px-2 py-1 rounded  hover:bg-green-800 focus-within:bg-slate-700 outline-none "
+            type="submit"
+          >
+            Registrar
+          </button>
+        </div>
+      </form>
     </>
   );
 }
